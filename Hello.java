b1a6665c3deb87c3a66464b2d852f10e437a19c3
Arfan Uddin <arfan.rfn@gmail.com>
2017-09-11 12:59:47 -0700

    Red leader made the 2nd change on redLeader fucntion

--- Hello.java
+++ Hello.java
@@ -13,7 +13,7 @@ public class Hello {
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         System.out.println("Lock S-foils in attack position.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-	System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
 
     }
 
@@ -27,6 +27,7 @@ public class Hello {
     public static void redLeader() {
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
+        System.out.println("I copy, gold leader!")
         
 
     }
